3ab4ac95d2f268263709a5474d6868307a312e63
Kevin Lago <kevinthelago@gmail.com>
2023-01-25 22:09:15 -0500

    Added Title and Legend

--- src/time_complexity_graph.py
+++ src/time_complexity_graph.py
@@ -11,24 +11,28 @@ def generate_polynomial_time_complexity_graph(resolution, darktheme):
     plt.setp(ax.spines.values(), color="white" if darktheme else "black")
     ax.set_facecolor("#0d1117" if darktheme else "white")
 
+    plt.title("Polynomial Time Complexity", c="white" if darktheme else "black")
     plt.xlabel("n", c="white" if darktheme else "black")
     plt.ylabel("time", c="white" if darktheme else "black")
 
     plt.xticks([])
     plt.yticks([])
 
+    linear_sequence = [i for i in range(resolution)]
+
     # Quadratic Runtime
-    plt.plot([i for i in range(resolution)], [math.pow(i, 2) for i in range(resolution)])
+    plt.plot(linear_sequence, [math.pow(i, 2) for i in range(resolution)], label="Quadratic Runtime")
     # Quasilinear Runtime
-    plt.plot([i for i in range(resolution)], [i * math.log2(i + 1) for i in range(resolution)])
+    plt.plot(linear_sequence, [i * math.log2(i + 1) for i in range(resolution)], label="Quasilinear Runtime")
     # Linear Runtime
-    plt.plot([i for i in range(resolution)], [i for i in range(resolution)])
+    plt.plot(linear_sequence, linear_sequence, label="Linear Runtime")
     # Logarithmic Runtime
-    plt.plot([i for i in range(resolution)], [math.log2(i + 1) for i in range(resolution)])
+    plt.plot(linear_sequence, [math.log2(i + 1) for i in range(resolution)], label="Logarithmic Runtime")
     # Constant Runtime
-    plt.plot([i for i in range(resolution)], [1 for _ in range(resolution)])
+    plt.plot(linear_sequence, [1 for _ in range(resolution)], label="Constant Runtime")
 
     plt.savefig("polynomial_time_complexity_graph" + ("_dark.svg" if darktheme else "_light.svg"))
+    plt.legend()
     plt.show()
 
 
